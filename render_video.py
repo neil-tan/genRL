ed335361e3f732f969e7082a126096602639ab49
Neil Tan <neil@utensor.ai>
2025-02-12 20:12:18 +0800

    cartpole slider doesn't fall anymore, but the pole seems to be frozen

--- render_video.py
+++ render_video.py
@@ -34,7 +34,7 @@ def main():
     )
     cartpole = scene.add_entity(
         # gs.morphs.MJCF(file='xml/franka_emika_panda/panda.xml'),
-        gs.morphs.URDF(file='assets/urdf/cartpole.urdf'),
+        gs.morphs.URDF(file='assets/urdf/cartpole.urdf', fixed=True),
     )
 
     cam = scene.add_camera(
@@ -48,10 +48,11 @@ def main():
     scene.build()
     
     # jnt_names = [j.name for j in cartpole.joints]
-    jnt_names = ['slider_to_cart', 'cart_to_pole']
+    # jnt_names = ['slider_to_cart', 'cart_to_pole']
+    jnt_names = ['slider_to_cart']
     dofs_idx = [cartpole.get_joint(name).dof_idx_local for name in jnt_names]
     # cartpole.set_pos(np.array([0, 0, 10]))
-    cartpole.control_dofs_force(np.array([10,0]), dofs_idx)
+    cartpole.control_dofs_velocity(np.array([-5]), dofs_idx)
     
     if not sys.platform == "linux":
         if sys.platform == "darwin" and scene._visualizer._viewer is not None:
@@ -61,14 +62,21 @@ def main():
         run_sim(scene, cam)
     scene.viewer.start()
 
+def rotate_cam_pose(i, distance=3, angular_velocity_scaler=1, fps=60, flip=False):
+    angle = i / fps * angular_velocity_scaler
+    angle = -angle if flip else angle
+    x = distance * np.sin(angle)
+    y = distance * np.cos(angle)
+    return (x, y, 2)
+
 
 def run_sim(scene, cam):
     cam.start_recording()
-    angular_velocity_scaler = 0.5
+    
     for i in range(300):
         scene.step()
         cam.set_pose(
-            pos    = (3.0 * np.sin(i / 60 * angular_velocity_scaler), 3.0 * np.cos(i / 60 * angular_velocity_scaler), 2.5),
+            pos    = rotate_cam_pose(i, distance=10, angular_velocity_scaler=0.5),
             lookat = (0, 0, 0.5),
         )
         cam.render()
