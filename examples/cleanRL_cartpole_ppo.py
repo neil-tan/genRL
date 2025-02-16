8f44d432ab7d5738c3d4ee0c8553bf568b5b7f62
Neil Tan <neil@utensor.ai>
2025-02-16 22:06:31 +0800

    cleanRL PPO signle environment

--- examples/cleanRL_cartpole_ppo.py
+++ examples/cleanRL_cartpole_ppo.py
@@ -1,11 +1,11 @@
 import genRL.gym_envs.genesis.cartpole as gen_cartpole
 import gymnasium as gym
 import torch
-import torch.nn as nn
-import torch.nn.functional as F
-import torch.optim as optim
 from torch.distributions import Categorical
 from genRL.rl.ppo import PPO
+import genesis as gs
+import sys
+import time
 
 #Hyperparameters
 learning_rate = 0.0005
@@ -15,14 +15,8 @@ eps_clip      = 0.1
 K_epoch       = 3
 T_horizon     = 20
 
-def main():
-    custom_environment_spec = gym.envs.registration.EnvSpec(id='my_env/gen_cartpole-v1', 
-                                                    entry_point=gen_cartpole.GenCartPoleEnv,
-                                                    reward_threshold=2000, 
-                                                    max_episode_steps=2000,
-                                                    )
-    env = gym.make(custom_environment_spec, render_mode="ansi", max_force=1000, targetVelocity=5)
 
+def training_loop(env):
     model = PPO()
     score = 0.0
     print_interval = 20
@@ -49,8 +43,24 @@ def main():
         if n_epi%print_interval==0 and n_epi!=0:
             print("# of episode :{}, avg score : {:.1f}".format(n_epi, score/print_interval))
             score = 0.0
-
+        
     env.close()
+
+def main():
+    env = gym.make("GenCartPole-v0", render_mode="human", max_force=1000, targetVelocity=5)
+
+    if not sys.platform == "linux":
+        gs.tools.run_in_another_thread(fn=training_loop, args=(env,))
+    else:
+        training_loop(env, 300)
+
+    time.sleep(1)
+    while True:
+        env.render()
+        # if env.unwrapped.done == True:
+        #     break
+        # time.sleep(1)
+    
 
 if __name__ == '__main__':
     main()
